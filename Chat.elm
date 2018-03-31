a8cb5d4e8c688e74f8c14469f8bcf542e4c2c05a
neodevelop <neodevelop@gmail.com>
2018-03-30 22:43:53 -0600

    Fix WS url

--- Chat.elm
+++ Chat.elm
@@ -47,7 +47,7 @@ update msg { input, messages } =
             ( Model newInput messages, Cmd.none )
 
         Send ->
-            ( Model "" messages, WebSocket.send "ws://echo.websocket.org" input )
+            ( Model "" messages, WebSocket.send "wss://echo.websocket.org" input )
 
         NewMessage str ->
             ( Model input (str :: messages), Cmd.none )
@@ -59,7 +59,7 @@ update msg { input, messages } =
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    WebSocket.listen "ws://echo.websocket.org" NewMessage
+    WebSocket.listen "wss://echo.websocket.org" NewMessage
 
 
 
