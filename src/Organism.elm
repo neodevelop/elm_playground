9562b0e48e143517015517d9307b8906511a344d
neodevelop <neodevelop@gmail.com>
2018-06-17 15:40:00 -0500

    Init organism

--- src/Organism.elm
+++ src/Organism.elm
@@ -3,3 +3,12 @@ module Organism exposing (..)
 
 type alias Cells =
     List (List Int)
+
+
+init : Cells
+init =
+    [ [ 0, 0, 0, 0 ]
+    , [ 0, 0, 0, 0 ]
+    , [ 0, 0, 0, 0 ]
+    , [ 0, 0, 0, 0 ]
+    ]
